db5dd88f82e6dc1eff8f1bf1eef212f70a033002
Barr Covrigaru <barr@covrigaru.com>
2019-01-23 14:26:10 -0700

    got psql file showing up in localhost browser

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const env = 'development'
+const config = require('./knexfile')[env]
+const knex = require('knex')(config)
+
+const sql = knex('notecards').toString()
+console.log(sql)
+
+app.get('/', (req, res, next) => {
+  knex.select('*').from('test')
+  // knex('methods') - secret shorthand
+  .then((rows) => {
+    res.send(rows)
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
